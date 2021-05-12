f4ab799492a1193852f93e6962ab9595baf6f011
unknown <jac_genato@yahoo.com>
2021-05-12 22:25:09 +0800

    Updates

---
+++ precache-manifest.fa09b3b1c69471e1e9f843ce015be25c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a25c4ae90edbc69ddd8da948796b8d5",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "f976532a1a08969bc1e8",
+    "url": "/github-finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/github-finder/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "f976532a1a08969bc1e8",
+    "url": "/github-finder/static/js/main.db6c471e.chunk.js"
+  },
+  {
+    "revision": "a06ceeb70725f86afdce",
+    "url": "/github-finder/static/js/runtime~main.cb74c248.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c9c68f93434abbceeaf1cc122dc195f.js"
+  "/github-finder/precache-manifest.fa09b3b1c69471e1e9f843ce015be25c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-finder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
