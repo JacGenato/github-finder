7c1375181d77f902acebe61bacf245cf3b04c7d6
unknown <jac_genato@yahoo.com>
2021-05-12 22:28:37 +0800

    Updates

---
+++ precache-manifest.a7af30d7790b57e346acb6251999d1e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed7aea901bf67b9c0c235ac866766b3",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "24adcd7c7d3214f22f56",
+    "url": "/github-finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/github-finder/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "24adcd7c7d3214f22f56",
+    "url": "/github-finder/static/js/main.6c02aa1b.chunk.js"
+  },
+  {
+    "revision": "a06ceeb70725f86afdce",
+    "url": "/github-finder/static/js/runtime~main.cb74c248.js"
+  }
+]);
